8d38b22fbc3840875eb4eb448017572d755c2418
Arsal Ajaz <ajazarsal2@gmail.com>
2021-10-05 12:32:37 -0700

    added firebase library

---
+++ src/js/firebase.js
@@ -0,0 +1,18 @@
+
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA6p0oIOiyyJA5Fj8lMqaFzexmMl6VF4hE",
+    authDomain: "project-1-e8317.firebaseapp.com",
+    databaseURL: "https://project-1-e8317-default-rtdb.firebaseio.com",
+    projectId: "project-1-e8317",
+    storageBucket: "project-1-e8317.appspot.com",
+    messagingSenderId: "425605597135",
+    appId: "1:425605597135:web:a720d9c0ec69f8920634ba",
+    measurementId: "G-WSKB4W094E"
+};
+const firebase = initializeApp(firebaseConfig);
+
+console.log(firebase);
+
